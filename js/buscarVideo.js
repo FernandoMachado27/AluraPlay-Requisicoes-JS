1b77e1d698ca86357cc3b290d0926ec6bb0f154a
FernandoMachado27 <fernando2747@yahoo.com>
2024-04-02 23:41:04 -0300

    Reutilizar funções

--- js/buscarVideo.js
+++ js/buscarVideo.js
@@ -1,4 +1,5 @@
 import { conectaApi } from "./conectaApi.js";
+import constoiCard from "./mostrarVideos.js";
 
 async function buscarVideo(evento) {
     evento.preventDefault();
@@ -6,7 +7,10 @@ async function buscarVideo(evento) {
     const dadosDePesquisa = document.querySelector("[data-pesquisa]").value;
     const busca = await conectaApi.buscaVideo(dadosDePesquisa);
 
-    console.log(busca);
+    const lista = document.querySelector("[data-lista]");
+
+    busca.array.forEach(elemento => lista.appendChild(
+        constoiCard(elemento.titulo, elemento.descricao, elemento.url, elemento.imagem))) // criou um card para cada elemento da lista
 }
 
 const botaoDePesquisa = document.querySelector("[data-botao-pesquisa]");
